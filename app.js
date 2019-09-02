21bf1f3437dfb1b990cd7f1315bd730775fe8a55
Jukka S <jukkas@gmail.com>
2019-09-02 16:51:11 +0300

    Add Helmet. Use Express bodyparser. app.js updates

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-'use strict';
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var swig = require('swig-templates');
-var session = require('cookie-session');
-var expressValidator = require('express-validator');
+const express = require('express');
+const helmet = require('helmet');
+const createError = require('http-errors');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const swig = require('swig-templates');
+const session = require('cookie-session');
+const expressValidator = require('express-validator');
 
-var routes = require('./routes/index');
-var sessionRoute = require('./routes/session');
+const routes = require('./routes');
+const sessionRoute = require('./routes/session');
 
-var app = express();
+const app = express();
 
-app.disable('x-powered-by');
+app.use(helmet());
+
 // view engine setup
 app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +25,8 @@ app.set('view cache', false); // Swig will cache templates
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('[:date] :remote-addr :method :url :status :res[content-length] ":referrer" ":user-agent"'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(expressValidator(sessionRoute.customValidators));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,10 +39,26 @@ app.use(session({
 }))
 
 // Routes
-app.get('/health', function (req, res) {
+app.get('/health', (req, res) => {
   res.send('OK');
 });
 
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
